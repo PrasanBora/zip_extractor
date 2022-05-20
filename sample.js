efd144622b05cef9ce08c049da2d2668f3b4f8eb
PrasanBora <prasanbora23@gmal.com>
2022-05-21 04:04:04 +0530

    firdt commit

---
+++ sample.js
@@ -0,0 +1,51 @@
+var JSZip = require("jszip");
+
+
+var $result = document.querySelector("#result");
+document.querySelector("#file").addEventListener("change", function(evt) {
+   
+    $result.innerHTML=" ";     // remove content
+
+
+    // Closure to capture the file information.
+    function handleFile(f) {
+        var $fileContent = document.createElement('ul');
+    
+        let $title = document.createElement('h4');
+        $title.appendChild(document.createTextNode(f.name));
+        
+
+        console.log(f.name);
+        
+        $result.append($title);
+        $result.append($fileContent);
+
+        JSZip.loadAsync(f)                                   // 1) read the Blob
+        .then(function(zip) {
+           
+            
+            zip.forEach(function (relativePath, zipEntry) {
+                 
+                let linode = document.createElement('li');
+                linode.appendChild(document.createTextNode(zipEntry.name));
+                $fileContent.appendChild(linode);
+                
+                // console.log(zipEntry.name);
+            });
+        }, 
+        function (e) {
+            let divnode=  document.createElement('div');
+            divnode.appendChild(document.createTextNode("Error reading " + f.name ));
+            $result.appendChild(divnode);
+            
+         }
+        );
+    }
+
+    var files = evt.target.files;
+    for (var i = 0; i < files.length; i++) {
+        handleFile(files[i]);
+    }
+});
+//////////////////////////
+
